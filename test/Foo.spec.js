de9c3d16ab40fb484e2a21a678e7eac17d73e449
eddyerburgh <edward.yerburgh@gmail.com>
2018-08-19 15:21:02 +0100

    Replace avoriaz with Vue Test Utils

--- test/Foo.spec.js
+++ test/Foo.spec.js
@@ -1,26 +1,25 @@
-import { expect } from 'chai';
-import { mount } from 'avoriaz';
-import Foo from '../app/components/Foo.vue';
+import { expect } from 'chai'
+import { mount } from '@vue/test-utils'
+import Foo from '../app/components/Foo.vue'
 
 describe('Foo.vue', () => {
-   it('has a root element with class foo', () => {
-     const wrapper = mount(Foo);
-     expect(wrapper.is('.foo')).to.equal(true);
-   });
+  it('has a root element with class foo', () => {
+    const wrapper = mount(Foo)
+    expect(wrapper.is('.foo')).to.equal(true)
+  })
 
   it('has a root element with class foo', () => {
-    const passedProp = 'some text';
-    const wrapper = mount(Foo, {propsData: {passedProp}});
-    const p = wrapper.find('p')[0];
-    expect(p.text()).to.equal(passedProp);
-  });
+    const passedProp = 'some text'
+    const wrapper = mount(Foo, { propsData: { passedProp }})
+    const p = wrapper.find('p')
+    expect(p.text()).to.equal(passedProp)
+  })
 
   it('changes h1 text when #change-text is clicked', () => {
-    const wrapper = mount(Foo);
-    const changeMessage = wrapper.find('#change-message')[0];
-    changeMessage.dispatch('click');
-    const h1 = wrapper.find('h1')[0];
-
-    expect(h1.text()).to.equal('new message');
-  });
-});
+    const wrapper = mount(Foo)
+    const changeMessage = wrapper.find('#change-message')
+    changeMessage.trigger('click')
+    const h1 = wrapper.find('h1')
+    expect(h1.text()).to.equal('new message')
+  })
+})
